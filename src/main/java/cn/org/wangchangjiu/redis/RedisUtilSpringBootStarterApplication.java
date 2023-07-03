b682e033ab4962f36b94f5aa4ae0e1a3aae4bc2a
wangchangjiu <wangchangjiu@rr.tv>
2023-07-03 17:01:45 +0800

    初始化项目

---
+++ src/main/java/cn/org/wangchangjiu/redis/RedisUtilSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package cn.org.wangchangjiu.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisUtilSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisUtilSpringBootStarterApplication.class, args);
+    }
+
+}
